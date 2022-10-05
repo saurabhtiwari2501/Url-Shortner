ec81cb5613d1f4bbe5a80ae62f37fae17e3200a2
rajeshmajhi2233 <rajeshmajhi2233@gmail.com>
2022-10-05 19:58:42 +0530

    final

--- src/routers/route.js
+++ src/routers/route.js
@@ -3,17 +3,18 @@ const router = express.Router()
 const urlcontroller = require('../controllers/urlController')
 
 
-
-
 router.get("/test-me", function (req, res) {
     res.send("server is running ")
 })
-
 
 // ----------------------------- CREATE URL --------------------------------------
 router.post("/url/shorten", urlcontroller.createUrl)
 // ----------------------------- GET URL -----------------------------------------
 router.get('/:urlCode', urlcontroller.getUrl)
 
+//----------------------------Error Handing----------------------
+router.all('/*', (req, res) => {
+    res.status(404).send({ status: false, message: "URL Not Found" })
+})
 
 module.exports = router
